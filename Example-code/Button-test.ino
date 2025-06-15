d39a9346e1bae9ec7195dd6f88e3e5edf1d578da
ViggoVH <viggovanhoutte@gmail.com>
2025-06-15 16:29:07 +0200

    Finalized test script, requires testing

--- Example-code/Button-test.ino
+++ Example-code/Button-test.ino
@@ -1,6 +1,4 @@
-/* BrightDot example program by PSI @ Velleman NV */
-/* Based on the NeoPixel Ring simple sketch by Shae Erisson */
-/* Adapted by Helger2007 as examplecode for the ARC-platform project */
+/* Example sketch to test connectors by Helger2007 as examplecode for the ARC_platform project */
 
 //***DECLARATIONS
 //NEOPIXEL library
@@ -10,23 +8,31 @@
 #endif
 
 // Which pin on the Arduino is connected to the BrightDots?
-// On a VMW100 this is 12, on VMW101 this is 19
+// On a VMW100 it's 12
 #define PIN            12
 
 // How many BrihgtDots are attached to the Arduino?
-// On a VMW100 this is 24
+// 24 on a VMW100
 #define NUMPIXELS      24
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
+// Setting up the Neopixel library using the previously defined number of LED's and pin.
+// The last parameter might need to be changed according to hardware.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int delayval = 500; // delay for half a second
 
-//***SETUP (run once) 
+//***SETUP (runs once)
+// Define the pinMode for every pin that needs to be tested.
 void setup() 
 {
   pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(4,INPUT);
+  pinMode(5,INPUT);
+  pinMode(6,INPUT);
+  pinMode(7,INPUT);
+  pinMode(8,INPUT);
+  pinMode(9,INPUT);
+  pinMode(10,INPUT);
+  pinMode(11,INPUT);
   pixels.begin();
 }
 
@@ -34,14 +40,85 @@ void setup()
 //***LOOP 
 void loop() 
 {
-  int buttonState = digitalRead(2);
-  
-  if (buttonState == LOW) {
-    pixels.setPixelColor(0,pixels.Color(255, 0, 0));
-    pixels.show();
+  int buttonState2 = digitalRead(2);
+  int buttonState3 = digitalRead(3);
+  int buttonState4 = digitalRead(4);
+  int buttonState5 = digitalRead(5);
+  int buttonState6 = digitalRead(6);
+  int buttonState7 = digitalRead(7);
+  int buttonState8 = digitalRead(8);
+  int buttonState9 = digitalRead(9);
+  int buttonState10 = digitalRead(10);
+  int buttonState11 = digitalRead(11);
+
+  if (buttonState2 == LOW) {
+    pixels.setPixelColor(1,pixels.Color(255, 0, 0));
   }
   else {
-    pixels.setPixelColor(0,pixels.Color(0, 0, 0));
-    pixels.show();
+    pixels.setPixelColor(1,pixels.Color(0, 0, 0));
   }
+
+  if (buttonState3 == LOW) {
+    pixels.setPixelColor(2,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(2,pixels.Color(0, 0, 0));
+  }
+
+  if (buttonState4 == LOW) {
+    pixels.setPixelColor(3,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(3,pixels.Color(0, 0, 0));
+  }
+
+  if (buttonState5 == LOW) {
+    pixels.setPixelColor(4,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(4,pixels.Color(0, 0, 0));
+  }
+
+  if (buttonState6 == LOW) {
+    pixels.setPixelColor(5,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(5,pixels.Color(0, 0, 0));
+  }
+
+  if (buttonState7 == LOW) {
+    pixels.setPixelColor(6,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(6,pixels.Color(0, 0, 0));
+  }
+
+  if (buttonState8 == LOW) {
+    pixels.setPixelColor(7,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(7,pixels.Color(0, 0, 0));
+  }
+
+  if (buttonState9 == LOW) {
+    pixels.setPixelColor(8,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(8,pixels.Color(0, 0, 0));
+  }
+
+  if (buttonState10 == LOW) {
+    pixels.setPixelColor(9,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(9,pixels.Color(0, 0, 0));
+  }
+
+  if (buttonState11 == LOW) {
+    pixels.setPixelColor(10,pixels.Color(255, 0, 0));
+  }
+  else {
+    pixels.setPixelColor(10,pixels.Color(0, 0, 0));
+  }
+  pixels.show()
 }
