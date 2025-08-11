1bf2227cb1e8d3ec92e907e2a3e3131c98007ae0
ViggoVH <viggovanhoutte@gmail.com>
2025-08-11 14:31:51 +0200

    finalized connector test and top cover stl

--- Example-code/Connector_test/Connector_test.ino
+++ Example-code/Connector_test/Connector_test.ino
@@ -20,19 +20,12 @@
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 //***SETUP (runs once)
-// Define the pinMode for every pin that needs to be tested.
+// Define the pin that needs to be tested in the variable below.
+#define TEST 10
+
 void setup() 
 {
-  pinMode(2,INPUT);
-  pinMode(3,INPUT);
-  pinMode(4,INPUT);
-  pinMode(5,INPUT);
-  pinMode(6,INPUT);
-  pinMode(7,INPUT);
-  pinMode(8,INPUT);
-  pinMode(9,INPUT);
-  pinMode(10,INPUT);
-  pinMode(11,INPUT);
+  pinMode(TEST,INPUT);
   pixels.begin();
 }
 
@@ -40,85 +33,11 @@ void setup()
 //***LOOP 
 void loop() 
 {
-  int buttonState2 = digitalRead(2);
-  int buttonState3 = digitalRead(3);
-  int buttonState4 = digitalRead(4);
-  int buttonState5 = digitalRead(5);
-  int buttonState6 = digitalRead(6);
-  int buttonState7 = digitalRead(7);
-  int buttonState8 = digitalRead(8);
-  int buttonState9 = digitalRead(9);
-  int buttonState10 = digitalRead(10);
-  int buttonState11 = digitalRead(11);
-
-  if (buttonState2 == LOW) {
-    pixels.setPixelColor(1,pixels.Color(255, 0, 0));
+  if (digitalRead(TEST) == HIGH) {
+    pixels.setPixelColor(0,pixels.Color(255, 0, 255));
   }
   else {
-    pixels.setPixelColor(1,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState3 == LOW) {
-    pixels.setPixelColor(2,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(2,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState4 == LOW) {
-    pixels.setPixelColor(3,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(3,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState5 == LOW) {
-    pixels.setPixelColor(4,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(4,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState6 == LOW) {
-    pixels.setPixelColor(5,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(5,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState7 == LOW) {
-    pixels.setPixelColor(6,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(6,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState8 == LOW) {
-    pixels.setPixelColor(7,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(7,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState9 == LOW) {
-    pixels.setPixelColor(8,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(8,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState10 == LOW) {
-    pixels.setPixelColor(9,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(9,pixels.Color(0, 0, 0));
-  }
-
-  if (buttonState11 == LOW) {
-    pixels.setPixelColor(10,pixels.Color(255, 0, 0));
-  }
-  else {
-    pixels.setPixelColor(10,pixels.Color(0, 0, 0));
+    pixels.setPixelColor(0,pixels.Color(0, 0, 0));
   }
   pixels.show();
 }
